68b8c4c3269bac7b00b845b6c8133122a305d374
ivnmelendez <87046117+ivnmelendez@users.noreply.github.com>
2026-06-05 21:01:55 -0600

    feat: initial Xcode project setup

---
+++ municipiosn/municipiosnApp.swift
@@ -0,0 +1,17 @@
+//
+//  municipiosnApp.swift
+//  municipiosn
+//
+//  Created by ivn on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct municipiosnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
